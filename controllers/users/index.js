0df355599d15fd60acc493c1fa7d97bfb1d589b4
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2022-06-07 02:12:30 +0530

    developed create user and login user

---
+++ controllers/users/index.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcryptjs")
+const config = require("config")
+const jwt = require("jsonwebtoken")
+
+const User = require("../../models/User")
+
+const userController = {
+    userCreate: async (req, res) => {
+        try {
+            const { email, password } = req.body
+
+            const user = new User({
+                email,
+                password
+            })
+            const salt = await bcrypt.genSalt(10)
+            user.password = await bcrypt.hash(password, salt)
+            const result = await user.save()
+
+            const payLoad = {user: {"id": user._id}}
+            const token = jwt.sign(payLoad, config.get("tokenSecretKey"), {expiresIn: 240000})
+
+            res.status(200).send({"Message": "Successfully created a user", "Data": result, "Token": token})
+
+        } catch (error) {
+            res.status(400).send({"Message": "Error while creating a user", "Data": error.message})
+        }
+    },
+
+    userLogin: async (req, res) => {
+        try {
+            const { email, password } = req.body
+            let user = await User.findOne( {email} ) //
+
+            if(!user) {
+                return res.status(400).send({"Message": "Invalid credential"})
+            }
+
+            const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+            if(!isPasswordMatch) {
+                return res.status(400).send({"Message": "Invalid credential"})
+            }
+
+            const payLoad = {user: {id: user._id}}
+            const token = jwt.sign(payLoad, config.get("tokenSecretKey"), {expiresIn: 240000})
+
+            return res.status(200).send({"Message": "Successfully logged in", "Data": user, "Token": token})
+            // return res.status(200).send({"Message": "Successfully logged in", "Token": token})
+
+        } catch (error) {
+            console.log(error.message);
+            return res.status(400).send({"Message": "Error occurred while log in"})
+        }
+    }
+}
+
+module.exports = userController
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+
+const userRoute = require("./users")
+
+router.use("/user", userRoute)
+
+module.exports = router
