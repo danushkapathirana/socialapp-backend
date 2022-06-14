3568bf2667195f2c1df0a80f03ab4526537c6a36
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2022-06-14 22:52:38 +0530

    developed post comment and delete comment api

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -9,5 +9,8 @@ router.get("/", middleware, postsController.fetchAllPosts)
 router.get("/:id", middleware, postsController.fetchPostById)
 router.delete("/:id", middleware, postsController.deletePost)
 router.put("/like/:id", middleware, postsController.likePost)
+router.post("/comment/:id", middleware, postsController.postComment)
+router.delete("/comment/:postId/:commentId", middleware, postsController.deleteComment)
+// router.put("/comment/:postId/:commentId", middleware, postsController.updateComment)
 
 module.exports = router
